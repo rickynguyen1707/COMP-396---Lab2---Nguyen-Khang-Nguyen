8bce7144cc65dd0b91fc830806decbd2f799f8e1
Ricky <nguyenkhanglhp1605@gmail.com>
2021-09-22 07:44:58 -0400

    Lab 2 completed

---
+++ Assets/NPCController.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+    //Simple FSM (FiniteStateMachine)
+    public enum NPCState {
+        Patrolling,
+        Chasing,
+        Attacking
+    };
+
+
+    public float CloseEnoughDistance = 2; //<=2 m => attack
+    //To be able to simulate different states
+
+    public NPCState currentState = NPCState.Patrolling;
+    public bool bCanSeePlayer;
+    [SerializeField]
+    GameObject goPlayer;
+
+    //New as of Sep.20th
+    GameObject[] Waypoints;
+    int CurrentWaypointIndex = 0;
+    public float speed = 1; //1 m/s
+    //--------------------------
+
+
+
+    //float closenessToWaypoint=.1; //<.1 m => we are at waypoint
+    void ChangeState(NPCState newState)
+    {
+        currentState = newState;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //First method: use tag Waypoint
+        Waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+
+        //Second method: get Waypoints' parent and then get all children (Waypoints)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleFSM();
+        
+    }
+    void HandleFSM()
+    {
+        switch (currentState)
+        {
+            case NPCState.Patrolling:
+                HandlePatrollingState();
+                break;
+            case NPCState.Chasing:
+                HandleChasingState();
+                break;
+            case NPCState.Attacking:
+                HandleAttackingState();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void HandleAttackingState()
+    {
+        Debug.Log("In NPCController.HandleAttackingState");
+    }
+
+    private void HandleChasingState()
+    {
+        //Debug.Log("In NPCController.HandleChasingState");
+
+
+        if (!bCanSeePlayer)
+        {
+            ChangeState(NPCState.Patrolling);
+        }
+
+        this.transform.position = MyMoveTowards(this.transform.position, goPlayer.transform.position, speed * Time.deltaTime);
+
+
+    }
+
+    private void HandlePatrollingState()
+    {
+        Debug.Log("In NPCController.HandlePatrollingState");
+        if (bCanSeePlayer)
+        {
+            ChangeState(NPCState.Chasing);
+        }
+
+        FollowPatrolingPath();
+    }
+
+    private void FollowPatrolingPath()
+    {
+        //if in waypoint => calculate nextwaypointindex
+        //else, go to currentwaypointindex;
+
+        //Mind different patrolling strategies
+        // . follow in order
+        // . follow randomly 
+        Vector3 target = Waypoints[CurrentWaypointIndex].transform.position;
+        if (Vector3.Distance(this.transform.position, target) < .1)
+        {
+            CurrentWaypointIndex = CalculateNextWaypointIndex();
+            target=Waypoints[CurrentWaypointIndex].transform.position;
+        }
+        // speed m/s
+        // d=v*dt  [m/s]*[s] => [m]
+        //Vector3 movement = Vector3.MoveTowards(this.transform.position, target, speed * Time.deltaTime);
+        Vector3 movement = MyMoveTowards(this.transform.position, target, speed * Time.deltaTime);
+        this.transform.position = movement;
+        
+    }
+
+    Vector3 MyMoveTowards(Vector3 current, Vector3 target, float maxDistanceDelta)
+    {
+        Vector3 C2T = target - current;
+
+        //-------- Start of Changes related to Rotation -------------
+        //this.transform.LookAt(target); //Abrupt1: this is a too abrupt rotation; see also Abrupt2 below
+
+        Quaternion qtargetrotation = Quaternion.LookRotation(C2T);
+        //this.transform.rotation = qtargetrotation;  //Abrupt2: this is a too abrupt rotation; not very beleivable; to try, uncomment this an comment out the line below
+
+        // This is the smothest rotation; more beleivable
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, qtargetrotation, Time.deltaTime);
+        //-------- End of Changes related to Rotation -------------
+
+        Vector3 movement = current + C2T.normalized * maxDistanceDelta;
+        return movement;
+
+    }
+
+    private int CalculateNextWaypointIndex()
+    {
+        //Strategy 1 - follow in order
+        return CurrentWaypointIndex=(CurrentWaypointIndex+1) % Waypoints.Length;
+
+
+
+ 
+        //throw new NotImplementedException();
+    }
+
+    private void CanSeeAdvesary()
+    {
+        //throw new NotImplementedException();
+
+        //GameObject goPlayer = GameObject.FindGameObjectWithTag("Player");
+        //
+        Vector3 playerPos = goPlayer.transform.position;
+        Vector3 E2P_Heading = playerPos - this.transform.position;
+        float cosAngleE2P = Vector3.Dot(this.transform.forward, E2P_Heading) / E2P_Heading.magnitude;
+        //float cosAngleE2P = Vector3.Dot(this.transform.forward, E2P_Heading) ; //we need only the sign of cosAngle, so no need to devide by a >0 size (a small optimization)
+        bCanSeePlayer = (cosAngleE2P > 0); //we are assuming FoV=180 degrees; if 
+        
+        
+        float angle = Vector3.Angle(this.transform.forward, E2P_Heading);
+        Debug.Log("angle=" + angle);
+        //return bCanSeePlayer; //for testing purpuses
+
+
+
+        //cos (theta)=v1.v2/(|v1|*|v2|)
+        //if v1 is a unit vector => |v1|=1   //forward= (0,0,1) initially and it is always unit vector (with size 1)\
+        //so are right, up;  left=-right, down=-up, back=-forward => they are also unit vectors
+         //this.transform
+    }
+
+    private void FixedUpdate()
+    {
+        CanSeeAdvesary();
+    }
+
+    private void OnDrawGizmos()
+    {   //Path
+        Gizmos.color = Color.green;
+        if (Waypoints.Length > 0) {
+            for (int i = 0; i < Waypoints.Length; i++)
+            {
+                Vector3 from = Waypoints[i].transform.position;
+                Vector3 to = Waypoints[(i + 1) % Waypoints.Length].transform.position;
+                Gizmos.DrawLine(from, to);
+            }
+        }
+
+    }
+}
---
+++ Assets/TestVectorsAndMatrices.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestVectorsAndMatrices : MonoBehaviour
+{
+    public GameObject gov1, gov2;
+    public Vector3 v1, v2;
+    public float K= 1.5f;
+    public GameObject goCubePrefab;
+
+    public GameObject goCubev1, goCubev2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        v1 = new Vector3(-1.0f, 2.5f, 1.5f);
+        v2 = new Vector3(1.0f, -2.5f, +3.5f);
+
+        
+
+        Debug.Log("v1=" + v1);
+        Debug.Log("v2=" + v2);
+
+        gov1.transform.position = v1;
+        gov2.transform.position = v2;
+
+        goCubev1=Instantiate(goCubePrefab, v1, Quaternion.identity);
+        goCubev2=Instantiate(goCubePrefab, v2, Quaternion.identity);
+
+        goCubev1.name = "CubeV1";
+        goCubev2.name = "CubeV2";
+
+        Debug.Log(v1 + v2);
+        Debug.Log(v2 + v1); //should be the same
+        Debug.Assert(v1 + v2 == v2 + v1);
+
+        Debug.Log(v2 - v1);
+        Debug.Log(v1 - v2); //should be with opposite signs
+        Debug.Assert(v1-v2 == (-1) * (v2-v1));
+
+        Debug.Log(K*v1);
+        Debug.Log(K*v2);
+
+        float v1_v2 =Vector3.Dot(v1, v2);
+        float v2_v1 = Vector3.Dot(v2, v1);
+        Debug.Assert(v1_v2 == v2_v1);
+
+        Vector3 v1xv2 = Vector3.Cross(v1, v2);
+        Vector3 v2xv1 = Vector3.Cross(v2, v1);
+        Debug.Assert(v1xv2 == (-1)*v2xv1);
+
+        //linear interpolation
+        // t0, t1  and two values y0,y1 =>
+        // for t in [t0,t1] we can find y in [y0,y1] such that
+        // trhe point (t,y) is in the line from (t0,y0) to (t1,y1)
+        //how:
+        // (t-t0)/(t1-t0)=(y-y0)/(y1-y0) =>
+        // y =lerp(p0,p1,t)
+        //   = y0+(t-t0)/(t1-t0)*(y1-y0)
+
+        //If t0==0, and t1==1 =>
+        //lerp01(y0,y1,t)=y0+t*(y1-y0)
+
+        //lerp01(v0,v1,t)=v0+t*(v1-v0)=(1-t)*v0+t*v1
+
+        float t = .25f;
+        Vector3 v = v1 + t * (v2 - v1);
+        //Vector3 v_m = Mathf.Lerp(v0 + t * (v1 - v0);
+        Vector3 v_m = Vector3.Lerp(v1, v2, t); // 0 + t * (v1 - v0);
+        Debug.Assert(v == v_m);
+
+
+        Debug.Log(v1.magnitude);
+        Debug.Log(v2.magnitude);
+        //Vector3.
+
+        // Heading_C_T =Target-Current;
+
+        ////Lab at Home: Due before class next monday
+        //Given two Vectors:
+        //v1 = (1.5, 2.5, 3.5)  and v1 = (1.0, -3.5, 1.5)
+
+        //and a constant
+
+        //K = 0.75
+
+        //Calculate the following quantities:
+
+        //v1 + v2
+        //v1 - v2
+
+        //K* v1
+        //K* v2
+
+        //v1.v2(dot product)
+
+        //v1 x v2(cross product)
+
+        //| v1 | (magnitude or size of v1)
+        //| v2 | (magnitude or size of v2)
+
+        //angle(v1, v2)(the angle between v1 and v2: Hint: find y = cos(angle) first, then the angle with Mathf.acos(y))
+
+
+        //From notes: cos(angle(v1, v2)) =v1.v2 / | v1 | *| v2 |
+
+        //(Do this with both methods: this one and Mathf.Angle method and compare them).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
